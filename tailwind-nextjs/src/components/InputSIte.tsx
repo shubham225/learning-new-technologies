b084b2de69565b29d84d0f9ebcbb31724be4f8af
Shubham Shinde <shubhamshinde225@gmail.com>
2025-04-21 22:15:51 +0530

    Minor changes

--- tailwind-nextjs/src/components/InputSIte.tsx
+++ tailwind-nextjs/src/components/InputSIte.tsx
@@ -22,7 +22,12 @@ export default function InputDemo() {
             <option value="wss://">wss://</option>
           </select>
           <span className="pointer-events-none absolute inset-y-0 end-0 z-10 flex h-full w-9 items-center justify-center text-muted-foreground/80 peer-disabled:opacity-50">
-            <ChevronDown size={16} strokeWidth={2} aria-hidden="true" role="img" />
+            <ChevronDown
+              size={16}
+              strokeWidth={2}
+              aria-hidden="true"
+              role="img"
+            />
           </span>
         </div>
         <Input
